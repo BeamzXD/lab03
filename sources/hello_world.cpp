9d5bd1cc2cebc4032ee4a05f120dec29a94f4016
BeamzXD <makes.k.s@mail.ru>
2024-04-20 14:17:47 +0300

    Apply clang-format with Mozilla style

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,12 +1,15 @@
 // hello_world.cpp
 // Simple program to greet the user
 
-#inculde <iostream>
+#inculde < iostream>
 
-int main(){
-	std::string name;
-	std::cout << "Enter your name: "; // Prompting user for name
-	std::cin >> name; // Taking input from user
-	std::cout << "Hello world from " << name << "!" << std::endl; // Printing greeting message
-	return 0;
+int
+main()
+{
+  std::string name;
+  std::cout << "Enter your name: "; // Prompting user for name
+  std::cin >> name;                 // Taking input from user
+  std::cout << "Hello world from " << name << "!"
+            << std::endl; // Printing greeting message
+  return 0;
 }
