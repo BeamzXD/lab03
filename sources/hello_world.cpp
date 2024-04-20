ef9fb54d6e69951d220eb521031cc89f8299ffb9
BeamzXD <makes.k.s@mail.ru>
2024-04-20 13:57:24 +0300

    Added comments to hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,12 @@
+// hello_world.cpp
+// Simple program to greet the user
+
 #inculde <iostream>
 
 int main(){
 	std::string name;
-	std::cout << "Enter your name: ";
-	std::cin >> name;
-	std::cout << "Hello world from " << name << "!" << std::endl;
+	std::cout << "Enter your name: "; // Prompting user for name
+	std::cin >> name; // Taking input from user
+	std::cout << "Hello world from " << name << "!" << std::endl; // Printing greeting message
 	return 0;
 }
